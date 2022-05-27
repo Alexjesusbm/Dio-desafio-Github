13976ef3c7499daa158fe5732ff6fd7d017da8d6
alexjesusbm <alexjesusbm@gmail.com>
2022-05-27 18:05:15 -0300

    contador javascript web

---
+++ assets/js/scripts.js
@@ -0,0 +1,21 @@
+var currentNumberinicio = document.getElementById('currentNumber')
+var currentNumber = 0
+
+function increment(){
+    currentNumber = currentNumber + 1;
+    currentNumberinicio.innerHTML = currentNumber;
+}
+function decrement(){
+    currentNumber = currentNumber - 1;
+    currentNumberinicio.innerHTML = currentNumber;
+}
+
+
+
+
+
+
+
+
+
+
